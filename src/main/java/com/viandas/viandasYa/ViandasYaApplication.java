73f25af418faf70c5c6818a6074c23394fcafecb
OttoOctavius <OttoOctavius@users.noreply.github.com>
2019-09-03 00:00:37 -0300

    Creacion de proyecto con dependencias incluidas

---
+++ src/main/java/com/viandas/viandasYa/ViandasYaApplication.java
@@ -0,0 +1,13 @@
+package com.viandas.viandasYa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViandasYaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViandasYaApplication.class, args);
+	}
+
+}
