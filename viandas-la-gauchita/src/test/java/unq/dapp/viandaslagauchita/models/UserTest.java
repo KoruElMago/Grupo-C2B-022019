d43d3f2d260866b6a655692cfa9c8df10291e682
Javier Corbalan <jcorbalan@devartis.com>
2019-09-10 18:01:03 -0300

    added first test

---
+++ viandas-la-gauchita/src/test/java/unq/dapp/viandaslagauchita/models/UserTest.java
@@ -0,0 +1,29 @@
+package unq.dapp.viandaslagauchita.models;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserTest {
+
+    @Mock
+    Address address;
+
+    @Test
+    public void canCreateUser() {
+        User user = User.builder()
+                .name("foo")
+                .address(address)
+                .telphone("12312312")
+                .email("mail_test@tes.com")
+                .build();
+
+        Assert.assertEquals("foo", user.getName());
+
+    }
+
+    ;
+}
