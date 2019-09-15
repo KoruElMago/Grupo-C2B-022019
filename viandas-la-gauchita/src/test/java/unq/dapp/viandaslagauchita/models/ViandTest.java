7c1c776efb94f9ab8006faaf83ff74ebcf2f9457
OttoOctavius <OttoOctavius@users.noreply.github.com>
2019-09-15 01:14:37 -0300

    Se modifico la clase para instanciar el objeto

--- viandas-la-gauchita/src/test/java/unq/dapp/viandaslagauchita/models/ViandTest.java
+++ viandas-la-gauchita/src/test/java/unq/dapp/viandaslagauchita/models/ViandTest.java
@@ -1,14 +1,39 @@
 package unq.dapp.viandaslagauchita.models;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+//@ContextConfiguration
+//@TestExecutionListeners( { DependencyInjectionTestExecutionListener.class,
+//        StepScopeTestExecutionListener.class })
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ViandTest {
 
-    @Test
-    public void testVacio(){
+    private Set<String> categorias;
 
+    @Before
+    public void setUp(){
+        categorias = new HashSet<>();
+        categorias.add("Sushi");
+    }
+
+    @Test
+    public void testCreacionDeVianda(){
+        Viand vianda = new Viand("Vianda",
+                             "Corporativa",
+                                categorias,
+                             15f,
+                            Date.from(Instant.MIN),Date.from(Instant.MAX),
+                            20f, 20,
+                            30f , 30);
+
+        //assertEquals
     }
 }
